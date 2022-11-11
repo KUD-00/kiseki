6807110faffe677438c0ed8f6d883d6f57833c8b
KUD <kasa7qi@gmail.com>
2022-11-11 21:47:50 +0900

    changge package.json

--- next.config.js
+++ next.config.js
@@ -9,7 +9,16 @@ module.exports = nextConfig
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-  // webpack: (config, { isServer }) => {
+  // webpack5: true,
+  // webpack: (config) => {
+  //   config.resolve.fallback = {
+  //     fs: false,
+  //     child_process: false
+  //   };
+
+  //   return config;
+  // },
+  // // webpack: (config, { isServer }) => {
   //   // 空のオブジェクト渡すことでnpmパッケージがfsモジュールに依存しないようにします
   //   if (!isServer) {
   //     config.node = {
