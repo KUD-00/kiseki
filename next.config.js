1e21a26fbcbaa7e3ab13ad03e3674dd6cbd1d956
KUD <kasa7qi@gmail.com>
2022-11-11 21:31:23 +0900

    Try to delete webpack config to solve the problem

--- next.config.js
+++ next.config.js
@@ -9,15 +9,15 @@ module.exports = nextConfig
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config, { isServer }) => {
-    // 空のオブジェクト渡すことでnpmパッケージがfsモジュールに依存しないようにします
-    if (!isServer) {
-      config.node = {
-        fs: 'empty',
-        net: 'empty',
-        tls: 'empty'
-      }
-    }
-    return config
-  }
+  // webpack: (config, { isServer }) => {
+  //   // 空のオブジェクト渡すことでnpmパッケージがfsモジュールに依存しないようにします
+  //   if (!isServer) {
+  //     config.node = {
+  //       fs: 'empty',
+  //       net: 'empty',
+  //       tls: 'empty'
+  //     }
+  //   }
+  //   return config
+  // }
 }
